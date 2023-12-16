5747fc76ff0b892f9958bf8f48abb684317fbb8c
GGSWiftPro <radoslav_petroff@abv.bg>
2023-12-16 21:18:38 +0200

    Creating DB and Linking it

--- pairEmployee/src/main/java/com/sirmaexam/pairEmployee/PairEmployeeApplication.java
+++ pairEmployee/src/main/java/com/sirmaexam/pairEmployee/PairEmployeeApplication.java
@@ -3,10 +3,37 @@ package com.sirmaexam.pairEmployee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class PairEmployeeApplication {
 
 	public static void main(String[] args) {
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "7005038450");
+			statement = connection.createStatement();
+			statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname = 'EmployeePair'");
+			ResultSet resultSet = statement.getResultSet();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			if (count <= 0) {
+				statement.executeUpdate("CREATE DATABASE EmployeePair");
+			}
+		} catch (SQLException e) {
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+			}
+		}
 		SpringApplication.run(PairEmployeeApplication.class, args);
 	}
 
