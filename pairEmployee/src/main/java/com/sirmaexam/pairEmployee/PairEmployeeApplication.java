97de2ef5b2d3515a1db2c53044886dcc2983243b
GGSWiftPro <radoslav_petroff@abv.bg>
2023-12-16 20:33:24 +0200

    Project Creation

---
+++ pairEmployee/src/main/java/com/sirmaexam/pairEmployee/PairEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.sirmaexam.pairEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairEmployeeApplication.class, args);
+	}
+
+}
---
+++ pairEmployee/src/test/java/com/sirmaexam/pairEmployee/PairEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirmaexam.pairEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
